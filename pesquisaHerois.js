fc69d6bd53e2632c565bffc89670073caa5b5848
heroandvillain <44068234+heroandvillain@users.noreply.github.com>
2018-10-11 15:20:07 -0300

    Update pesquisaHerois.js

--- pesquisaHerois.js
+++ pesquisaHerois.js
@@ -18,8 +18,8 @@ window.onload = function(){
 request.open('GET', url, true);
 
 request.setRequestHeader('Access-Control-Allow-Headers', '*');
-//request.setRequestHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-//request.setRequestHeader("Access-Control-Allow-Methods", "GET, PUT, POST");
+request.setRequestHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+request.setRequestHeader("Access-Control-Allow-Methods", "GET, PUT, POST");
 request.setRequestHeader("Access-Control-Allow-Origin", "*");
 
 request.open('GET', url, true);
