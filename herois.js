719af33a3cac25c4c6296640a1c8a9a1d8c916e3
searchimages <44068234+searchimages@users.noreply.github.com>
2018-11-08 14:22:30 -0200

    Update herois.js

--- herois.js
+++ herois.js
@@ -5,6 +5,11 @@ var passaValor= function(valor){
 }
  passaValor(document.getElementById('nomeHeroi').value);
 }
-
-
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode !== 13) return;
+    	var passaValor= function(valor){
+    		window.location = "pages/search.html?nome="+valor;
+		}
+		passaValor(document.getElementById('nomeHeroi').value);
+});
 
