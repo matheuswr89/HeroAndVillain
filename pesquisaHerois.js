0f923659d9226007bcdbe61701cf96cf750d5fad
searchimages <44068234+searchimages@users.noreply.github.com>
2018-11-08 14:21:58 -0200

    Update pesquisaHerois.js

--- pesquisaHerois.js
+++ pesquisaHerois.js
@@ -1,62 +1,55 @@
-
-
 window.onload = function(){
 	var nome = this.queryString("nome");
-	  console.log(nome);
-
-
-
- 	var table =document.getElementById("tab");
-	var tbody = document.getElementById("tbo");
-	var div = document.getElementById("power");
-	var retorno = new Object();
+ 	var element = document.getElementById('element');
+ 	var power = document.getElementById('power');
+  var pesquisa = document.getElementById('pesquisa');	
 
   var request = new XMLHttpRequest();
-
-  var url = 'Access-Control-Allow-Origin: <http://superheroapi.com/api/2195914800646269/search/>' + nome;
-
-request.open('GET', url, true);
+  
+request.open('GET', 'http://www.superheroapi.com/api.php/2195914800646269/search/' + nome , true);
 request.onload = function () {
 
   var data = JSON.parse(this.response);
-  retorno = data;
   if (request.status >= 200 && request.status < 400) {
   	
-   tbody.innerHTML="";
+   pesquisa.innerHTML="";
+    
+  	element.innerHTML +="<h2>resultados para " +nome+"</h2>";
+  	power.innerHTML +="Resultados para " +nome;
 
   try {
-        for(var i=0; i<=retorno.results.length; i++){
-          tbody.innerHTML += "<tr><td> <center><img src=" +retorno.results[i].image.url + " class='imgherois'></center>" +
-           "<tr><td>"+"Nome:"+retorno.results[i].name +    
-           "<tr><td>"+ "Nome Real:"+retorno.results[i].biography["full-name"] +   
-           "<tr><td>"+ "Alter-Ego:"+retorno.results[i].biography["alter-egos"] +   
-           "<tr><td>"+ "Aliados:"+retorno.results[i].biography.aliases +   
-           "<tr><td>"+ "Naturalidade:"+retorno.results[i].biography["place-of-birth"] +   
-           "<tr><td>"+ "Apareceu pela primeira vez em:"+retorno.results[i].biography["first-appearance"] +   
-           "<tr><td>"+ "Universo:"+retorno.results[i].biography.publisher +   
-           "<tr><td>"+"Do Bem ou do Mau:"+retorno.results[i].biography.alignment+    
-           "<tr><td>"+"Inteligencia: " + "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.intelligence+"%>"+retorno.results[i].powerstats.intelligence+"</div></div>"+ 
-           "<tr><td>"+"Força:"+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.strength+"%>"+retorno.results[i].powerstats.strength+"</div></div>"+ 
-           "<tr><td>"+"Velocidade:"+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.speed+"%>"+retorno.results[i].powerstats.speed+"</div></div>"+ 
-           "<tr><td>"+"Durabilidade:"+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.durability+"%>"+retorno.results[i].powerstats.durability+"</div></div>"+ 
-           "<tr><td>"+"Poder:"+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.power+"%>"+retorno.results[i].powerstats.power+"</div></div>"+ 
-           "<tr><td>"+"Combate:"+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+retorno.results[i].powerstats.combat+"%>"+retorno.results[i].powerstats.combat+"</div></div>"+   
-           "<tr><td>"+"Genero:"+retorno.results[i].appearance.gender+    
-           "<tr><td>"+"Raça:"+retorno.results[i].appearance.race+    
-           "<tr><td>"+"Altura:"+retorno.results[i].appearance.height+    
-           "<tr><td>"+"Peso:"+retorno.results[i].appearance.weight+    
-           "<tr><td>"+"Cor dos Olhos:"+retorno.results[i].appearance["eye-color"]+    
-           "<tr><td>"+"Cor dos Cabelos:"+retorno.results[i].appearance["hair-color"]+    
-           "<tr><td>"+"Oucupação:"+retorno.results[i].work.occupation+    
-           "<tr><td>"+"Base:"+retorno.results[i].work.base+    
-           "<tr><td>"+"Afiliação:"+retorno.results[i].connections["group-affiliation"]+    
-           "<tr><td>"+"Parentes:"+retorno.results[i].connections.relatives+"</td></tr>";    
+        for(var i=0; i<=data.results.length; i++){
+          pesquisa.innerHTML += " <center><img src=" +data.results[i].image.url + " class='imgherois'></center>" +
+           ""+"Nome: " +data.results[i].name +    
+           "<br>"+ "Nome Real: " +data.results[i].biography["full-name"] +   
+           "<br>"+ "Alter-Ego: " +data.results[i].biography["alter-egos"] +   
+           "<br>"+ "Aliados: " +data.results[i].biography.aliases +   
+           "<br>"+ "Naturalidade: " +data.results[i].biography["place-of-birth"] +   
+           "<br>"+ "Apareceu pela primeira vez em: " +data.results[i].biography["first-appearance"] +   
+           "<br>"+ "Universo: " +data.results[i].biography.publisher +   
+           "<br>"+"Do Bem ou do Mau: " +data.results[i].biography.alignment+    
+           "<br>"+"Inteligencia: " + "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.intelligence+"%>"+data.results[i].powerstats.intelligence+"</div></div>"+ 
+           "<br>"+"Força: "+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.strength+"%>"+data.results[i].powerstats.strength+"</div></div>"+ 
+           "<br>"+"Velocidade: "+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.speed+"%>"+data.results[i].powerstats.speed+"</div></div>"+ 
+           "<br>"+"Durabilidade: "+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.durability+"%>"+data.results[i].powerstats.durability+"</div></div>"+ 
+           "<br>"+"Poder: "+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.power+"%>"+data.results[i].powerstats.power+"</div></div>"+ 
+           "<br>"+"Combate: "+ "<div class='porcentagem porc'><div class='por cor porc' + style=width:"+data.results[i].powerstats.combat+"%>"+data.results[i].powerstats.combat+"</div></div>"+   
+           "<br>"+"Genero: " +data.results[i].appearance.gender+    
+           "<br>"+"Raça: " +data.results[i].appearance.race+    
+           "<br>"+"Altura: " +data.results[i].appearance.height+    
+           "<br>"+"Peso: " +data.results[i].appearance.weight+    
+           "<br>"+"Cor dos Olhos: " +data.results[i].appearance["eye-color"]+    
+           "<br>"+"Cor dos Cabelos: " +data.results[i].appearance["hair-color"]+    
+           "<br>"+"Oucupação: " +data.results[i].work.occupation+    
+           "<br>"+"Base: " +data.results[i].work.base+    
+           "<br>"+"Afiliação: " +data.results[i].connections["group-affiliation"]+    
+           "<br>"+"Parentes: " +data.results[i].connections.relative;    
 
 	     } 
   } catch (e) {
      //console.log(e);
-     if (retorno.results == null) {
-     	tbody.innerHTML += "<tr><td>"+"O Herói ou Vilão não foi encontrado!!! =("+"</td><td>";
+     if (data.results == null) {
+     	pesquisa.innerHTML += ""+"O Herói ou Vilão não foi encontrado!!! =(";
      }
     return undefined;
   }
