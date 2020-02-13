d21fcaf92f12fd91553e9f85bfd42ce687aef3c6
Matheus William <matheuswr89@outlook.com>
2020-02-13 15:21:11 -0300

    Add files via upload

---
+++ js/top.js
@@ -0,0 +1,16 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    document.getElementById("footer-inicio").style.position = "relative";
+  } else {
+    mybutton.style.display = "none";
+    document.getElementById("footer-inicio").style.position = "absolute";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
