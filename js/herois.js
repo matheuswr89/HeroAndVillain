4285d8e0bc4ac83497af338d3992b7c76d407fce
Matheus William <matheuswr89@outlook.com>
2020-02-28 16:42:07 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -22,7 +22,7 @@ function consultar() {
                     try {
                         var coll = document.getElementsByClassName("collapsible");
                         for (var i = 0; i < coll.length; i++) {
-                            coll[i].addEventListener("click", function () {
+                            coll.addEventListener("click", function () {
                                 this.classList.toggle("active");
                                 var content = this.nextElementSibling;
                                 if (content.style.maxHeight) {
