8ec984048225a72042cebe9f05b150064e3faeff
Matheus William <matheuswr89@outlook.com>
2020-04-03 21:13:26 -0300

    Update herois.js

--- js/herois.js
+++ js/herois.js
@@ -22,17 +22,17 @@ function consultar() {
                     try {
                         
                             var coll = document.getElementsByClassName("collapsible");
-                            for (var i = 0; i < coll.length; i++) {
-                                coll[i].addEventListener("click", function () {
-                                    this.classList.toggle("active");
-                                    var content = this.nextElementSibling;
-                                    if (content.style.maxHeight) {
-                                        content.style.maxHeight = null;
-                                    } else {
-                                        content.style.maxHeight = content.scrollHeight + "px";
-                                    }
-                                });
-                            }
+                            for (i = 0; i < coll.length; i++) {
+							  coll[i].addEventListener("click", function() {
+							    this.classList.toggle("active");
+							    var content = this.nextElementSibling;
+							    if (content.style.display === "block") {
+							      content.style.display = "none";
+							    } else {
+							      content.style.display = "block";
+							    }
+							  });
+							}
                             pesquisa.innerHTML +=
                                 `<button class="collapsible colapse">${data.name}</button>
                                         <div id="content">
@@ -145,4 +145,3 @@ document.addEventListener('keydown', function (event) {
     }
     passaValor(document.getElementById('nomeHeroi').value);
 });
-
