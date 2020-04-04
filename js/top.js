1aa13b707f762b8d6d41913165d0275c05e366b2
Matheus William <matheuswr89@outlook.com>
2020-04-03 21:25:57 -0300

    Update top.js

--- js/top.js
+++ js/top.js
@@ -2,13 +2,16 @@ var mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 21 || document.documentElement.scrollTop > 21) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
     document.getElementById("footer-inicio").style.position = "relative";
   } else {
     mybutton.style.display = "none";
-    document.getElementById("footer-inicio").style.position = "absolute";
   }
+  if(document.body.scrollTop<15 || document.documentElement.scrollTop <15){
+        document.getElementById("footer-inicio").style.position = "absolute";
+  }
+  
 }
 function topFunction() {
   document.body.scrollTop = 0;
