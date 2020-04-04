8f65ab6f48cd492cc5495844c42c56acac7aba33
Matheus William <matheuswr89@outlook.com>
2020-04-03 21:54:30 -0300

    Update top.js

--- js/top.js
+++ js/top.js
@@ -1,11 +1,11 @@
 let mybutton = document.getElementById("myBtn");
-let body = document.body, html = document.documentElement
-let docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-let winHeight = window.innerHeight;
 
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
+  let body = document.body, html = document.documentElement
+  let docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  let winHeight = window.innerHeight;
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
     document.getElementById("footer-inicio").style.position = "relative";
